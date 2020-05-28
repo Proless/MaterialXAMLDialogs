18f4964a9012a8e3eda8c7557b7c04b39175b215
Proless <61435433+Proless@users.noreply.github.com>
2020-05-28 14:00:41 +0200

    A class to be used to easily get hold of a Dialog session and interact with it.

---
+++ MaterialXAMLDialogs/DialogHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using MaterialDesignThemes.Wpf;
+
+namespace MaterialXAMLDialogs
+{
+	public class DialogHandler
+	{
+		// Properties
+		public DialogSession Session { get; private set; }
+		public object Result { get; private set; }
+
+		// Events
+		public event DialogOpenedEventHandler DialogOpened;
+		public event DialogClosingEventHandler DialogClosing;
+
+		// Methods
+		public Task<object> Show(object content)
+		{
+			return DialogHost.Show(content, OnDialogOpened, OnDialogClosing);
+		}
+		public Task<object> Show(object content, object dialogIdentifier)
+		{
+			return DialogHost.Show(content, dialogIdentifier, OnDialogOpened, OnDialogClosing);
+		}
+
+		// Event Handlers
+		private void OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
+		{
+			Result = eventArgs.Parameter;
+			DialogClosing?.Invoke(sender, eventArgs);
+		}
+		private void OnDialogOpened(object sender, DialogOpenedEventArgs eventArgs)
+		{
+			Session = eventArgs.Session;
+			DialogOpened?.Invoke(sender, eventArgs);
+		}
+	}
+}
